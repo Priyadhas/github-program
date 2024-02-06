2145f47124d7ff565d6a0545f73c634a593b2cad
PriyaDhas <mpriyad2001@gmail>
2024-02-06 15:48:00 +0530

    yes

--- average.java
+++ average.java
@@ -2,8 +2,8 @@ public class average {
 public static void main(String[] args)
 {
     
-    int a=4,b=7,j=6,n=4,h=5;
-    int average=(a+b+j+n+h)/5;
+    int a=4,b=7,j=6,n=4,t=5;
+    int average=(a+b+j+n+t)/5;
     System.out.println("The average is "+average);
 
 }
